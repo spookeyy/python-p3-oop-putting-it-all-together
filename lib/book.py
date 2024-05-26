c9516bd938af2f480624c831e3bd1cc21bf8358b
spookeyy <pangasmeshack@gmail.com>
2024-05-26 12:10:57 +0300

    tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+        self.set_page_count(page_count)
+
+    def set_page_count(self, page_count):
+        if not isinstance(page_count, int):
+            print("page_count must be an integer\n")
+        else:
+            self._page_count = page_count
+
+    def get_page_count(self):
+        return self._page_count
+            
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
-        
+
+# page = Book("The World According to Garp", 69)
+# print(page.page_count)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.set_size(size)
+
+    def set_size(self, size):
+        if not isinstance(size, int):
+            print("size must be an integer\n")
+        else:
+            self._size = size
+
+    def get_size(self):
+        return self._size
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    size = property(get_size, set_size)
